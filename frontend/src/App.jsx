6e3431d96f0497739bcc24f3e5603ba252b3f0ff
“bamiyanapp” <s_yosuke128@yahoo.co.jp>
2026-01-03 22:48:06 +0900

    chore: URLを更新

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useCallback, useEffect, useMemo, useRef } from "react";
 import "./App.css";
 
-const API_BASE_URL = "https://w9b74k1y0b.execute-api.ap-northeast-1.amazonaws.com/dev";
+const API_BASE_URL = "https://akmnirkx3m.execute-api.ap-northeast-1.amazonaws.com/dev";
 
 function App() {
   const [categories, setCategories] = useState([]);
