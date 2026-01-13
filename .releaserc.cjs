c48796be6bc9d7e7dd533de5d3a49513fa07b0d4
bamiyanapp <36719508+bamiyanapp@users.noreply.github.com>
2026-01-13 22:41:30 +0900

    Semantic patch 99 (#29)
    * fix: update .releaserc.cjs
    
    * fix: update cd.yml

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["main", "release"],
+  branches: ["main"],
   repositoryUrl: "https://github.com/bamiyanapp/uchi-stock.git",
   plugins: [
     [
@@ -7,20 +7,16 @@ module.exports = {
       {
         preset: "conventionalcommits",
         releaseRules: [
-          // 小文字（正規）
           { type: "feat", release: "minor" },
           { type: "fix", release: "patch" },
 
-          // 大文字対応
           { type: "Feat", release: "minor" },
           { type: "Fix", release: "patch" },
           { type: "Refactor", release: "patch" },
 
-          // スラッシュ系（今ある履歴対応）
           { type: "Feat/fix", release: "patch" },
           { type: "Fix/tech", release: "patch" },
 
-          // 明示的にリリースしない
           { type: "Docs", release: false },
           { type: "Chore", release: false }
         ],
@@ -28,38 +24,12 @@ module.exports = {
       }
     ],
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    [
-      "@semantic-release/npm",
-      {
-        "npmPublish": false
-      }
-    ],
-    [
-      "@semantic-release/exec",
-      {
-        prepareCmd: "node scripts/convert-changelog-to-json.js"
-      }
-    ],
     [
       "@semantic-release/github",
       {
-        "successComment": false,
-        "failCommentCondition": false,
-        "releasedLabels": false
-      }
-    ],
-    [
-      "@semantic-release/git",
-      {
-        assets: [
-          "CHANGELOG.md",
-          "frontend/src/changelog.json",
-          "package.json",
-          "package-lock.json"
-        ],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        successComment: false,
+        failCommentCondition: false,
+        releasedLabels: false
       }
     ]
   ]
